1c4b632e892a48a635a00ca94d54740886dffd45
wonchangkim <wonchang.k@gmail.com>
2017-11-17 17:03:35 +0900

    로그인 폼관련영역일부마크업 완성및 일부디자인수정

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -1,17 +1,23 @@
 $(document).ready(function() {
-  var menu = $('main-menu >li');
-  var subMenu = $('.sub-menu');
-  var last = $('.sub-menu li:last-child a');
+  var menu = $('.main-menu >li');
+  var animationbox = $('.visual>li')
 
   menu.hover(function() {
     $(this).find('.sub-menu').toggleClass('menu-act');
   });
-  menu.focus(function() {
-    $(this).find('.sub-menu').addClass('menu-act');
 
+  menu.focusin(function() {
+    $(this).siblings().find('.sub-menu').removeClass('menu-act');
+    $(this).find('.sub-menu').addClass('menu-act');
   });
-  last.focusout(function() {
-    $(this).parents('.sub-menu').removeClass('memu-act');
+  animationbox.on('click ' () {
+    $(this).toggleClass('box-act');
   });
+
+
 
 });
+
+// 문서가 다로딩되고 해석하기:헤딩에 선언되어있더라도 로딩다되고나서 해석하기
+// toggleClass('.manu-act') 안됨 . 앞에 class 있으므로
+// on 상황발생할때 익명함수는 뒤에 따라옴
